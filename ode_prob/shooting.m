0aee535d6c03393409527545a66467ad7dfa7be2
Tsi <caiqqqppp@163.com>
2024-03-08 18:38:56 +0800

    2024-03-08

--- ode_prob/shooting.m
+++ ode_prob/shooting.m
@@ -1,39 +1,48 @@
 function y = shooting(ds_func, bc, x, guess)
-%SHOOTING Summary of this function goes here
-%   Detailed explanation goes here
+% shooting method to solve boundary value problem
+% y" = f(x, y, y') x \in [a,b],
+% a_0y(a) - a_1y'(a) = \alpha,
+% b_0y(b) + b_1y'(b) = \beta.
+% choose Newton's method to optimize the guess
+
+% ds_func: function to solve
+% bc: [coef_a, alpha, coef_b, beta], boundary conditions
+% where coef_a = [a_0, a_1], coef_b = [b_0, b_1]
+% x: interval of time variable
+% guess: initial guess of s for c_0y(a) - c_1y'(a) = s
+% Latest Version by ZM on Mar-08-2024
+% Tested with shooting_test.m
 
 % unpack boundary conditions
 coef_a = bc(1:2); alpha = bc(3);
 coef_b = bc(4:5); beta = bc(6);
 
-% choose proper c0 and c1   
+% choose proper c_0 and c_1 to make c_0a_1 - c_1a_0 = 1 
 if coef_a(1)==0 && coef_a(2)~=0
     coef_c(2) = 0;
-    coef_c(1) = -1/coef_a(2);
+    coef_c(1) = 1/coef_a(2);
 elseif coef_a(1)~=0 && coef_a(2)==0
-    coef_c(2) = 1/coef_a(1);
+    coef_c(2) = -1/coef_a(1);
     coef_c(1) = 0;
 else
-    coef_c(2) = 2/coef_a(1);
-    coef_c(1) = 1/coef_a(2);
+    coef_c(2) = 1/coef_a(1);
+    coef_c(1) = 2/coef_a(2);
 end
 
 % shooting
-max_iter = 1000;
 tol = 1e-6;
 
-for iter = 1:max_iter
-    init = [coef_a(1)*guess-coef_c(1)*alpha, coef_c(2)*alpha-coef_a(2)*guess, coef_a(2), coef_a(1)];
+init = [coef_a(2)*guess-coef_c(2)*alpha; coef_a(1)*guess-coef_c(1)*alpha; coef_a(2); coef_a(1)];
+[~, y_shoot] = ode45(ds_func, x, init);
+y_shoot = y_shoot.';
+error = coef_b * y_shoot(1:2,end) - beta;
+while abs(error) >= tol
+    init = [coef_a(2)*guess-coef_c(2)*alpha; coef_a(1)*guess-coef_c(1)*alpha; coef_a(2); coef_a(1)];
     [~, y_shoot] = ode45(ds_func, x, init);
     y_shoot = y_shoot.';
     error = coef_b * y_shoot(1:2,end) - beta;
     D_error = coef_b * y_shoot(3:4,end);
-    
-    if abs(error) < tol
-        break;
-    else
-        guess = guess - error/D_error;
-    end
+    guess = guess - error/D_error;
 end
 
 y = y_shoot(1,:);
--- ode_prob/shooting_test.m
+++ ode_prob/shooting_test.m
@@ -8,7 +8,8 @@
 % where c_2 = \frac{1}{70}[8-12\sin(\ln 2) - 4\cos(\ln 2)]
 % and c_1 = \frac{11}{10}-c_2.
 % set step length h = 0.05
-% Latest Version by ZM on Mar-07-2024
+% Latest Version by ZM on Mar-08-2024
+% Tested successfully!
 
 
 clear; clc;
@@ -21,7 +22,7 @@ u_exact = exact_sol(x);
 
 % shooting
 bound = [coef_a, 1, coef_b, 2];
-shoot_u = shooting(@ds_fun, bound, x, 0);
+u_shoot = shooting(@ds_fun, bound, x, 0);
 
 % bvp4c
 guess_sol = [2 2];
@@ -29,29 +30,28 @@ solinit = bvpinit(x, guess_sol);
 sol = bvp4c(@bvpfun, @bc, solinit);
 u = deval(sol, x);
 
-
-plot(x,u(1,:),'+k',x,u_exact,'-r','MarkerSize',8)
+% Numerical Results
+plot(x,u(1,:),'+k', x,u_shoot,'ok', x,u_exact,'-r','MarkerSize',8)
+legend('shooting.m', 'bvp4c',  'Exact Solution', 'Location','southeast')
 xlabel x, ylabel u
+title("Computational Solution vs Analytical Solution")
+hold on
 
-function dy = bvpfun(x, u) % equation to solve
-dy(1) = u(2);
-dy(2) = -2.*u(2)./x + 2.*u(1)./x.^2 + sin(log(x))./x.^2;
+
+
+function dy = bvpfun(x, u) % equation to solve in bvp4c
+dy = [u(2); -2.*u(2)./x + 2.*u(1)./x.^2 + sin(log(x))./x.^2];
 end
 
-function dys = ds_fun(x, u)
-dys = zeros(4, 1);
-dys(1) = u(2);
-dys(2) = -2.*u(2)./x + 2.*u(1)./x.^2 + sin(log(x))./x.^2;
-dys(3) = u(4);
-dys(4) = -2.*u(4)./x + 2.*u(3)./x.^2;
+function dys = ds_fun(x, u) % equation to solve in shooting.m
+dys = [u(2); -2.*u(2)./x + 2.*u(1)./x.^2 + sin(log(x))./x.^2; u(4); -2.*u(4)./x + 2.*u(3)./x.^2];
 end
 
-
-function res=bc(ua, ub) % boundary conditions
+function res=bc(ua, ub) % boundary conditions for bvp4c
 res=[ua(1)-1; ub(1)-2];
 end
 
-function u = exact_sol(x) % analytic solution
+function u = exact_sol(x) % exact solution
 c2 = (8-12.*sin(log(2))-4.*cos(log(2)))/70;
 c1 = 1.1-c2;
 u = c1.*x + c2./x.^2 -0.3.*sin(log(x)) - 0.1.*cos(log(x));
